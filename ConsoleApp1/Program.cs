5bf3ee9172db8e734b5743c15e38bb68f4ba0625
kemenes.marton <kemenes.marton@blathy.info>
2023-11-06 10:39:04 +0100

    csak fiúk és lányok vannak hehe

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,18 @@
             Console.Write("Nemed: ");
             string nem = Console.ReadLine();
             Console.WriteLine($"Ez esetben, kedves {nev}, te egy {nem} vagy.");
+            if (nem == "fiu" || nem == "fiú")
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Szeretem a fiúkat!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Szeretem a lányokat!");
+            }
         }
     }
 }
