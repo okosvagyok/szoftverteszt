74b149151515199d4a33f0e8e2280a419c5af035
kemenes.marton <kemenes.marton@blathy.info>
2023-11-06 10:33:51 +0100

    Ellopja az adataid.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,9 @@
             Console.Write("Neved: ");
             string nev = Console.ReadLine();
             Console.WriteLine($"Szia, {nev}!");
+            Console.Write("Nemed: ");
+            string nem = Console.ReadLine();
+            Console.WriteLine($"Ez esetben, kedves {nev}, te egy {nem} vagy.");
         }
     }
 }
