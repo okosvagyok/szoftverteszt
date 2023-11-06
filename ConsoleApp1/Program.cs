6db97f1f9fac9ad8825225f788690b605e35a883
Kemenes Márton <kemenes.marton@bot.local>
2023-11-06 10:30:09 +0100

    Nevet kér és köszön illedelmesen.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Neved: ");
+            string nev = Console.ReadLine();
+            Console.WriteLine($"Szia, {nev}!");
         }
     }
 }
