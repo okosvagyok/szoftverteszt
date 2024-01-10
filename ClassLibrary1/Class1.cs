3abf763465612583ae818af0d341a8941934e315
okosvagyok <kemenes.marton@blathy.info>
2024-01-10 14:27:19 +0100

    páros vagy páratlan

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,18 +4,7 @@
     {
         bool EvenOdd(int number)
         {
-            Console.Write("Kérek egy számot: ");
-            number = int.Parse(Console.ReadLine());
-            if (number%2 == 0)
-            {
-                Console.WriteLine($"A megadott szám ({number}) páros.");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine($"A megadott szám ({number}) páratlan.");
-                return false;
-            }
+            return (number % 2 == 0);
         }
     }
 }
